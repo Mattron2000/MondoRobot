ddf7e121061c9bbe82724d7fbb46db829885e5d2
Mattron2000 <matteopalmieri.code@gmail.com>
2022-08-12 14:42:15 +0200

    miglirato lo script javadoc per linux

--- makeJavadoc.sh
+++ makeJavadoc.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# script shell di javadoc per i linuxiani
-exec javadoc -package ./src/mondo_robot/*.java
+# #
+# #	script shell di javadoc per i linuxiani
+# #
+
+#	creo il processo per creare javadoc nell'apposita cartella
+exec javadoc -d ./javadoc/ -package ./src/mondo_robot/MVC_Main.java ./src/mondo_robot/View/*.java ./src/mondo_robot/Controller/*.java ./src/mondo_robot/Model/*.java &
+
+#	memorizzo il processo parallelo per poter aspettare la sua conclusione
+javadocPID=$
+wait $javadocPID
+
+#	concluso la generazione della documentazione javadoc, 
+#	apro la pagine index.html con il browser di default tramite xdg-utils 
+#	di www.freedesktop.org/wiki/Software/xdg-utils/
+exec xdg-open ./javadoc/index.html
